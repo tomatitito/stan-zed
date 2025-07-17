9836bbd4a71e3f6390725853dcaa4b6a31f898eb
Jens Kouros <jens.kouros@posteo.de>
2025-07-17 06:44:44 +0200

    Add language_server_command

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use zed::{Command, LanguageServerId, Result, Worktree};
 use zed_extension_api as zed;
 
 struct Stan;
@@ -6,6 +7,29 @@ impl zed::Extension for Stan {
     fn new() -> Self {
         println!("[STAN EXTENSION] Hello, world!");
         Stan
+    }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<Command> {
+        let binary_path = worktree.which("stan-language-server");
+
+        match binary_path {
+            Some(path) => {
+                eprintln!("[STAN EXTENSION]stan-language-server found at {path}");
+                Ok(Command {
+                    command: path,
+                    args: Default::default(),
+                    env: Default::default(),
+                })
+            }
+            None => {
+                eprintln!("[STAN EXTENSION] stan-language-server not found");
+                Err("stan-language-server not found".into())
+            }
+        }
     }
 }
 
